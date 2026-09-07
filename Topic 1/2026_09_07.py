7a6ade2d9da349ab855eae49bd551aa719d17e08
manki <ilyaklopov07@yandex.ru>
2026-09-07 23:17:04 +0300

    #00000001

---
+++ Topic 1/2026_09_07.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+
+n = 10000
+x = 50
+
+
+
+def factorial(n):
+    answer = 1
+    i = 1
+    while i < n:
+        i += 1
+        answer *= i
+
+    return answer
+
+def exp(x):
+    result = 0
+    for i in range(1, n):
+        result += x ** i / factorial(i)
+    return result
+
+print(np.exp(x), exp(x), np.exp(x) - exp(x))
