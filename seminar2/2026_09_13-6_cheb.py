0d8ac8179b33b3012f7690c41077b8ba509aed3c
manki <ilyaklopov07@yandex.ru>
2026-09-19 23:06:57 +0300

    #00000018

--- seminar2/2026_09_13-6_cheb.py
+++ seminar2/2026_09_13-6_cheb.py
@@ -4,19 +4,36 @@ import scipy as sp
 from scipy.interpolate import barycentric_interpolate
 import matplotlib.pyplot as plt
 
-def function(x):
+def f(x):
     return np.exp(3 * x) * np.sin(200 * x ** 2) / (1 + 20 * x ** 2)
 
+N = 10
+bins = np.linspace(-1, 1, 1000)
 
-observed_x, _ = sp.special.roots_chebyt(20)
-observed_y = function(observed_x)
-x = np.linspace(np.min(observed_x), np.max(observed_x), 1000)
-true_y = function(x)
-y = barycentric_interpolate(observed_x, observed_y, x)
-plt.plot(observed_x, observed_y, "o", label="observation")
-plt.plot(x, y, label="barycentric interpolation")
-plt.plot(x, true_y, label="function")
-plt.title(r'Интерполяция полинома $f=\frac{e^{3x}\sin{200x^2}}{1+20x^2}$ для 20 точек')
-plt.legend()
+points_range = np.arange(51, 201, 10)
+errors = np.array([])
+for k in points_range:
+    x = sp.special.roots_chebyt(k)[0]
+    y = barycentric_interpolate(x, f(x), bins)
+    error = np.max(np.abs(y - f(bins)))
+    errors = np.append(errors, error)
+
+x = sp.special.roots_chebyt(N)[0]
+y = barycentric_interpolate(x, f(x), bins)
+fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+
+ax[0].plot(x, f(x), 'o', label="points")
+ax[0].plot(bins, y, label="cheb")
+ax[0].plot(bins, f(bins), label="f(x)")
+
+ax[0].set_title(r'$f(x)=\sin(6x)+sign(\sin(x+e^{2x}))$ для 10 точек', loc='left')
+ax[0].legend()
+
+
+ax[1].plot(points_range, errors, label="error", color='red')
+ax[1].legend()
+ax[1].set_title(r'Ошибки при разном числе точек', loc='left')
+ax[1].set_xlabel('Число точек')
+ax[1].set_ylabel('Ошибка')
 plt.savefig('6_interpolation.png', dpi=300)
 plt.show()
